cf14957e4ff9538b035a943ff8548d4a65f60d97
Alberto <alberto.baezap@gmail.com>
2014-02-11 18:20:00 +0100

    Cambio de librerías a Dropbox Core API.
    Se ha cambiado la librería de Sync API a Core API ya que posee más
    funcionalidad en cuanto a búsqueda de ficheros y obtención de datos. Sin
    embargo la autentificación es más complicada.
    Hasta ahora se ha hecho un método sencillo de autentificación pero se
    han encontrado los siguientes problemas:
    
    - Si la aplicación no se cierra correctamente se pueden obtener errores
    de autentificación.
    
    TODO:
    
    - Implementar el almacenamiento de credenciales con SharedPreferences de
    manera que no haga falta iniciar sesión cada vez que se abre la
    aplicación.

--- BibliotecaDb/src/app/bq/bibliotecadb/MainActivity.java
+++ BibliotecaDb/src/app/bq/bibliotecadb/MainActivity.java
@@ -1,6 +1,8 @@
 package app.bq.bibliotecadb;
 
-import com.dropbox.sync.android.DbxAccountManager;
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.session.AppKeyPair;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -9,30 +11,67 @@ import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+
 	
 	// Elementos de conectividad con Dropbox.
-	private DbxAccountManager mDbxAcctMgr;
+	//private DbxAccountManager mDbxAcctMgr;
 	static final int REQUEST_LINK_TO_DBX = 0;
 	// Claves para la sincronización de la aplicación con Dropbox.
 	private static final String APP_KEY = "d7i4j7ctpt5hn1q";
 	private static final String APP_SECRET = "zxe1ea2972uyi62";
 
+	//Elementos de conectividad con Dropbox.
+	private AppKeyPair mAppKeys;
+	private AndroidAuthSession mSession;
+	private DropboxAPI<AndroidAuthSession> mApi;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        /*
         
         //Este método obtendrá una instancia de nuestra cuenta de Dropbox.
         mDbxAcctMgr = DbxAccountManager.getInstance(getApplicationContext(), APP_KEY, APP_SECRET);
         //Este método sincroniza la cuenta con Dropbox.
 		mDbxAcctMgr.startLink(MainActivity.this, REQUEST_LINK_TO_DBX);
+		
+		*/
+        
+        mAppKeys = new AppKeyPair(APP_KEY, APP_SECRET); //Guarda la clave y secreto de la API
+        mSession = new AndroidAuthSession(mAppKeys); //Guarda la sesiónd de autentificación
+        mApi = new DropboxAPI<AndroidAuthSession>(mSession); //Nueva instancia de sesión
+        mApi.getSession().startOAuth2Authentication(MainActivity.this); //Inicia intento de autentificación
+        
+        
     }
+    /**
+     * Después de una autentificación vuelve a onResume para terminar de cerrar el inicio
+     * de sesión correctamente.
+     */
+    
+    @Override
+    protected void onResume(){
+    		super.onResume();
+    		if(mApi.getSession().authenticationSuccessful()){
+    			try{
+    				mApi.getSession().finishAuthentication();
+    				Log.d("out"," Auth succesful");
+    				mSession.unlink();
+    			} catch (IllegalStateException e) {
+					e.printStackTrace();
+				}
+    		} else Log.d("out", "Auth failed");
+    		
+    }
+    
+    /*
     
     /**
      * La sincronización se realizará a través de una actividad externa por lo que el resultado
      * se tratará en onActivityResult() si la autentificación no da ningún error.
-     */
+     *
     @Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 	    if (requestCode == REQUEST_LINK_TO_DBX) {
@@ -47,6 +86,7 @@ public class MainActivity extends Activity {
 	        super.onActivityResult(requestCode, resultCode, data);
 	    }
 	}
+    */
 
 
     @Override
