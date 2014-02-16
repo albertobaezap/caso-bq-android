e1f612ec546fba4f1debbe7ed582b981a4a48051
Alberto <alberto.baezap@gmail.com>
2014-02-16 18:46:23 +0100

    Aplicación abierta para usuarios
    Llegado el tiempo límite, se ha decidido abrir la aplicación para el
    resto de usuarios. Para ello primero se ha editado la clave en la web de
    desarrolladores de Dropbox para poder abrirla a usuarios adicionales
    (hasta 100). En el caso de que la aplicación se publicara la restricción
    de usuarios desaparecería.
    
    También se ha cambiado un poco el aspecto visual de la lista, aunque no
    hubiera que tocar la interfaz, simplemente quedaba muy soso. Se ha
    añadido un fondo gradiente y un cambio de color al texto.
    
    Finalmente se proponen unas sencillas mejoras para la aplicación que
    ayudarían a su funcionamiento y que no se han terminado de implementar
    por falta de tiempo:
    
    - Ahora que está abierta a otros usuarios, se debería añadir una página
    de opciones para poder desvincular la aplicación de un usuario y poder
    acceder desde otra cuenta (mediante el método unlink()). Actualmente se
    puede hacer desde la web de Dropbox o desinstalando la aplicación.
    
    - En vez de obtener la lista cada vez que se abre la aplicación, se
    podría simplemente añadir un botón para "Sincronizar" los datos, y
    mientras tanto trabajar con la base de datos offline (en caso de
    necesitar una portada, si se requeriría una conexión). Actualmente
    mientras la aplicación esté corriendo, la sesión se encuentra abierta en
    todo momento.
    
    - Cuando vaya a descargar la lista por primera vez, primero observar el
    número de archivos a descargar, y añadir una barra de progreso indicando
    los archivos que faltan para terminar la descarga.
    
    Bugs conocidos o elementos a mejorar:
    
    - Actualmente la descarga de los archivos temporales se realiza en la
    raíz del directorio de almacenamiento externo, sería conveniente
    utilizar una carpeta apropiada.
    
    - La lista de elementos se recarga cada vez que se llama a onCreate() en
    la aplicación, es decir tanto si la aplicación vuelve de Background o
    cuando se cambia la orientación del teléfono, sería conveniente guardar
    el estado, forzar la no actualización, o en el peor caso, forzar la
    orientación Portrait mediante setRequestedOrientation. De momento se ha
    decidido dejar tal cual está.
    
    - Se ha trabajado con y sin la muestra de "Autor" en la lista, al final
    se ha quitado para trabajar con la orientación y no se ha vuelto a
    añadir por lo que en el código se encontrarán referencias a este
    parámetro.
    
    - A pesar de que cumple con las funcionalidades pedidas, falta testeo y
    prueba en varias cuentas, que no ha sido posible debido a la falta de
    tiempo.

--- BibliotecaDb/gen/app/bq/bibliotecadb/R.java
+++ BibliotecaDb/gen/app/bq/bibliotecadb/R.java
@@ -22,7 +22,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int book_icon=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int gradient=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int action_order=0x7f080004;
